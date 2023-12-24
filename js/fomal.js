3ed7888d07abb5bcfbf686252159f194bd19981f
hmn <2449806952@qq.com>
2023-12-24 22:26:14 +0800

    Site updated: 2023-12-24 22:26:13

--- js/fomal.js
+++ js/fomal.js
@@ -16,7 +16,7 @@ function tonav() {
     }
     position = scroll;
   });
-  // 修复没有弄右键菜单的童鞋无法回顶部的问题
+  // 修复没有弄右键菜单的童鞋无法回顶部的问题  
   document.getElementById("page-name").innerText = document.title.split(" | Shine's Blog🍓")[0];
 }
 
